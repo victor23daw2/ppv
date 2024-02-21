bb12bfc663ccf364dff947f2e7528eb3f2aa7982
victor23daw2 <458371.clot@fje.edu>
2024-02-21 01:38:08 +0100

    Commit 1 de l'aplicatiu ppv

---
+++ app/ppv.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>PRICE OF THE SELECTED MATCH</title>
+</head>
+        <body>
+                <p><u>PRICE OF THE SELECTED MATCH</u></p>
+                <?php
+			if ($_GET["e1"]==$_GET["e2"]){
+				echo "Wrong selection. A team can not play against itself. Select a different team 2, please</br>";
+			}
+			else{
+				if ((($_GET["e1"]=="b") and  ($_GET["e2"]=="d")) or (($_GET["e1"]=="d") and  ($_GET["e2"]=="b"))){
+					echo "Special match: Price: 13 Euros</br>";
+				}
+				else {
+					echo "Basic match. Price: 10 Euros</br>";
+				}
+			}
+		?>
+		<br>
+		<a href="index.html">Return to the init web page</a>
+        </body>
+</html>
